a8762cd754144bc28535dc6b6c0ccf55e2537595
AditiBR <53121538+AditiBR@users.noreply.github.com>
2020-02-19 22:08:49 -0800

    Added an interface iweatherservice. Also added service WeatherFakeService which implements this interface. Also existing service weatherservice extends from this interface

--- src/app/current-weather/current-weather.component.spec.ts
+++ src/app/current-weather/current-weather.component.spec.ts
@@ -2,6 +2,8 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CurrentWeatherComponent } from './current-weather.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { WeatherService } from '../weather.service';
+import { WeatherFakeService } from '../weather-fake.service';
 
 describe('CurrentWeatherComponent', () => {
   let component: CurrentWeatherComponent;
@@ -10,7 +12,8 @@ describe('CurrentWeatherComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ CurrentWeatherComponent ],
-      imports: [HttpClientTestingModule]
+      imports: [HttpClientTestingModule],
+      providers: [{provide: WeatherService, useClass: WeatherFakeService}]
     })
     .compileComponents();
   }));
---
+++ src/app/weather-fake.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { IWeatherService } from './iweather-service';
+import { ICurrentWeather } from './icurrent-weather';
+import { Observable, of } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherFakeService implements IWeatherService {
+
+  private fakeWeather: ICurrentWeather = {
+    city: 'Bellevue',
+    country: 'USA',
+    date: 1485789600,
+    image: '',
+    temperature: 280.32,
+    description: 'Light drizzle'
+    
+  }
+  constructor() { }
+
+  getCurrentWeather(city:string, country:string): Observable<ICurrentWeather>{
+  return of (this.fakeWeather);
+  }
+}
